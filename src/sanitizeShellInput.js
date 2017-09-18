695e7d475b276c2d678a32778febe19b77b898ec
MicCarr <miccarr@me.com>
2017-09-18 15:25:12 +0200

    Detection of FISH shell

--- src/sanitizeShellInput.js
+++ src/sanitizeShellInput.js
@@ -7,20 +7,21 @@ function sanitizeInput (input) {
     .split(endOfLine)
     .filter(line => !/^\s*$/.test(line))  // removes any empty lines
     .join(' && ');                        // change newlines into "&&"
-    
+
   // Fish shell && and || replacement
   try{
-    
-    const config = require('~/.hyper.js');
-    if( ~config.shell.search('fish') )
+
+    if( ~config.getConfig().shell.search('fish') )
       output = output
-        .replace(/\s?&&\s?/g, ' ;and ')  // Replace "&&" with ";and"
-        .replace(/\s?||\s?/g, ' ;or ');  // Replace "||" with ";or"
-    
-  } catch (err) {}
-  
+        .replace(/\s?\&\&\s?/g, ' ;and ')  // Replace "&&" with ";and"
+        .replace(/\s?\|\|\s?/g, ' ;or ');  // Replace "||" with ";or"
+
+  } catch (err) {
+    console.error(err);
+  }
+
   return output;
-  
+
 }
 
 module.exports = sanitizeInput;
